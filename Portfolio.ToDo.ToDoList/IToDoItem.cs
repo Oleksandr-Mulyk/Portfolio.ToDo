1fddd18419339c496ae5a17bfc84a9b9eb797895
Oleksandr Mulyk <mulyk.aleks@gmail.com>
2024-12-04 20:41:20 +0100

    Changed ToDoItems class to public

---
+++ Portfolio.ToDo.ToDoList/IToDoItem.cs
@@ -0,0 +1,13 @@
+﻿namespace Portfolio.ToDo.ToDoList
+{
+    public interface IToDoItem
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public bool IsComplete { get; set; }
+
+        public int SortOrder { get; set; }
+    }
+}
---
+++ Portfolio.ToDo.ToDoList/ToDoItem.cs
@@ -0,0 +1,13 @@
+﻿namespace Portfolio.ToDo.ToDoList
+{
+    public class ToDoItem : IToDoItem
+    {
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public bool IsComplete { get; set; }
+
+        public int SortOrder { get; set; }
+    }
+}
