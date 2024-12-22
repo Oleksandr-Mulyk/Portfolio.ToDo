f1d6f4bce43f0685d0ce22921eabbcd520cf39a6
Oleksandr Mulyk <mulyk.aleks@gmail.com>
2024-12-22 13:47:24 +0100

    Added ToDo Api Service

---
+++ Portfolio.ToDo.GRPC/Services/ToDoApiService.cs
@@ -0,0 +1,67 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Portfolio.ToDo.GRPC;
+using Portfolio.ToDo.GRPC.Data;
+using Portfolio.ToDo.ToDoList;
+
+namespace Portfolio.ToDo.GRPC.Services
+{
+    public class ToDoApiService(IToDoRepository toDoRepository) : ToDoService.ToDoServiceBase
+    {
+        public override async Task<ToDoItemProto> GetById(ToDoIdRequest toDoIdRequest, ServerCallContext context)
+        {
+            Guid id = Guid.Parse(toDoIdRequest.Id);
+            IToDoItem toDoItem = await toDoRepository.GetItemByIdAsync(id);
+
+            return await Task.FromResult(ToProto(toDoItem));
+        }
+
+        public override async Task<ToDoListResponse> GetAll(Empty emptyRequest, ServerCallContext context)
+        {
+            ToDoListResponse response = new();
+
+            IQueryable<IToDoItem> toDoItems = await toDoRepository.GetItemListAsync();
+
+            List<ToDoItemProto> toDoItemList = [.. toDoItems.Select(toDoItem => ToProto(toDoItem))];
+
+            response.Items.AddRange(toDoItemList);
+
+            return await Task.FromResult(response);
+        }
+
+        public override async Task<ToDoItemProto> Post(ToDoItemProto toDoItemProto, ServerCallContext context)
+        {
+            IToDoItem toDoItem = new ToDoItem
+            {
+                Id = Guid.Parse(toDoItemProto.Id),
+                Title = toDoItemProto.Title,
+                Description = toDoItemProto.Description,
+                IsComplete = toDoItemProto.IsComplete,
+                SortOrder = toDoItemProto.SortOrder
+            };
+            var result = await toDoRepository.SaveItemAsync(toDoItem);
+
+            return ToProto(result);
+        }
+
+        public override async Task<ToDoDeleteResponse> Delete(ToDoIdRequest toDoIdRequest, ServerCallContext context)
+        {
+            Guid id = Guid.Parse(toDoIdRequest.Id);
+            bool result = await toDoRepository.DeleteItemByIdAsync(id);
+
+            return new ToDoDeleteResponse() { Complete = result };
+        }
+
+        private static ToDoItemProto ToProto(IToDoItem toDoItem)
+        {
+            return new ToDoItemProto
+            {
+                Id = toDoItem.Id.ToString(),
+                Title = toDoItem.Title,
+                Description = toDoItem.Description,
+                IsComplete = toDoItem.IsComplete,
+                SortOrder = toDoItem.SortOrder
+            };
+        }
+    }
+}
