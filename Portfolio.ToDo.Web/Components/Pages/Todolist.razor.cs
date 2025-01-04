bb227e688829286639d3a2487511bdfbe573bff2
Oleksandr Mulyk <mulyk.aleks@gmail.com>
2025-01-04 15:15:15 +0100

    Added todo list in wep app

--- Portfolio.ToDo.Web/Components/Pages/Todolist.razor.cs
+++ Portfolio.ToDo.Web/Components/Pages/Todolist.razor.cs
@@ -6,9 +6,17 @@ namespace Portfolio.ToDo.Web.Components.Pages
 {
     public partial class Todolist(IToDoRepository repository) : ComponentBase
     {
-        private List<IToDoItem> _toDoItems;
+        private List<IToDoItem> _toDoItems = [];
+
+        private Guid? _expandedItemId;
 
         protected override async Task OnInitializedAsync() =>
             _toDoItems = [.. (await repository.GetItemListAsync())];
+
+        private void ToggleDetails(Guid itemId)
+        {
+            _expandedItemId = _expandedItemId == itemId ? null : itemId;
+            StateHasChanged();
+        }
     }
 }
